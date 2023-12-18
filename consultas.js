c6db64e9e9411f0f986c6b9d4579f24626765020
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-18 18:44:31 -0300

    Add getDate and agregarViaje functions

--- consultas.js
+++ consultas.js
@@ -7,3 +7,17 @@ const pool = new Pool({
     database: 'plan_de_viajes',
     allowExitOnIdle: true
 });
+
+const getDate = async () => {
+    const result = await pool.query('SELECT NOW()');
+    console.log(result);
+}
+getDate();
+
+const agregarViaje = async (destino, presupuesto) => {
+    const consulta = "INSERT INTO viajes values (DEFAULT, $1, $2)";
+    const values = [destino, presupuesto];
+    const result = await pool.query(consulta, values);
+    console.log("Viaje agregado");
+}
+agregarViaje('Cancun', 1000000);
